8bc59a6e191190bcf936180ade7317da69e1e645
Phisten <ans10528@gmail.com>
2019-08-29 17:35:04 +0800

    Revert "Revert "google map hybrid""
    This reverts commit 0544f4990c9d9eabfcfdf98034400b793e721c00.

--- src/QtLocationPlugin/QGeoTiledMappingManagerEngineQGC.cpp
+++ src/QtLocationPlugin/QGeoTiledMappingManagerEngineQGC.cpp
@@ -109,6 +109,8 @@ QGeoTiledMappingManagerEngineQGC::QGeoTiledMappingManagerEngineQGC(const QVarian
     mapTypes << QGCGEOMAPTYPE(QGeoMapType::StreetMap,         "Google Street Map",        "Google street map",            false,  false,  UrlFactory::GoogleMap);
     mapTypes << QGCGEOMAPTYPE(QGeoMapType::SatelliteMapDay,   "Google Satellite Map",     "Google satellite map",         false,  false,  UrlFactory::GoogleSatellite);
     mapTypes << QGCGEOMAPTYPE(QGeoMapType::TerrainMap,        "Google Terrain Map",       "Google terrain map",           false,  false,  UrlFactory::GoogleTerrain);
+    // try hybrid
+    mapTypes << QGCGEOMAPTYPE(QGeoMapType::HybridMap,        "Google Hybrid Map",       "Google hybrid map",           false,  false,  UrlFactory::GoogleHybrid);
 #endif
     mapTypes << QGCGEOMAPTYPE(QGeoMapType::StreetMap,         "GoogleChina Street Map",        "GoogleChina street map",            false,  false,  UrlFactory::GoogleChinaMap);
     mapTypes << QGCGEOMAPTYPE(QGeoMapType::SatelliteMapDay,   "GoogleChina Satellite Map",     "GoogleChina satellite map",         false,  false,  UrlFactory::GoogleChinaSatellite);
